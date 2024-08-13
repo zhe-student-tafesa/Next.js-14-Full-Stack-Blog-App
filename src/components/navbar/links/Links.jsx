d6e59c0e0d3a812a055daac924e39a3fa4e7ee12
AzureAD\FrankZhangMiningSkil <frank.zhang.sa.au@gmail.com>
2024-08-13 14:55:49 +1000

    1. add useState: for mobile menu open or not; MUST "use client" 2. parameter of setOpen: can be a funtion;

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,5 +1,6 @@
+"use client"
 import Link from 'next/link';
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './links.module.css'
 import NavLink from './navLink/NavLink';
 
@@ -25,18 +26,30 @@ const links = [
 const Links = () => {
     const session = true;
     const isAdmin = true;
+    const [open, setOpen] = useState(false);
     return (
-        <div className={styles.links}>
-            {links.map((link) =>
-                <NavLink link={link} key={link.title} />
-            )}{
-                session ? (
-                    <>
-                        {isAdmin && (<NavLink link={{ title: "Admin", path: "/admin" }} />)}
-                        <button className={styles.logout}>Logout</button>
-                    </>
-                )
-                    : (<NavLink link={{ title: "Login", path: "/login" }} />)
+        <div className={styles.container}>
+            <div className={styles.links}>
+                {links.map((link) =>
+                    <NavLink link={link} key={link.title} />
+                )}{
+                    session ? (
+                        <>
+                            {isAdmin && (<NavLink link={{ title: "Admin", path: "/admin" }} />)}
+                            <button className={styles.logout}>Logout</button>
+                        </>
+                    )
+                        : (<NavLink link={{ title: "Login", path: "/login" }} />)
+                }
+            </div>
+            <button onClick={() => setOpen((prev) => !prev)}>Menu</button>
+            {open && <div>
+                {
+                    links.map((link) =>
+                        <NavLink link={link} key={link.title} />
+                    )
+                }
+            </div>
             }
         </div>
     )
