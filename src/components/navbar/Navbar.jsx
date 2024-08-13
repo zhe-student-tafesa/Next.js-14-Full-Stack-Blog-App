8260952935dfb3f7c0d1e07d8faeb3357969892f
AzureAD\FrankZhangMiningSkil <frank.zhang.sa.au@gmail.com>
2024-08-13 13:16:39 +1000

    1. !! use   **.module.css to prevent css conflict; 2. finish Navbar layout;

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Links from './links/Links'
+import styles from './navbar.module.css'
 
 const Navbar = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <div>
         Logo
       </div>
