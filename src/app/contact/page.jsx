14f43217330a5b56075f53cc595181f0d93ad587
AzureAD\FrankZhangMiningSkil <frank.zhang.sa.au@gmail.com>
2024-08-14 13:45:07 +1000

    1. finish contact page;

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import styles from "./contact.module.css";
+import Image from 'next/image';
 
 const ContactPage = () => {
   return (
-    <div>ContactPage</div>
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image className={styles.img} src='/contact.png' fill />
+      </div>
+      <div className={styles.formContainer}>
+        <form className={styles.form} action=''>
+          <input placeholder='Name and Surname'></input>
+          <input placeholder='Email Address'></input>
+          <input placeholder='Phone Number (Optional)'></input>
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            placeholder="Message"
+          ></textarea>
+          <button>Send</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
