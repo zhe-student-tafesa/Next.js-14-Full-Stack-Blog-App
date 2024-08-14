3ed15272b9d882c43212734dfa5421c0ec149939
AzureAD\FrankZhangMiningSkil <frank.zhang.sa.au@gmail.com>
2024-08-14 11:05:21 +1000

    1. Finish home page layout;

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,24 @@
+import Image from 'next/image';
+import styles from './home.module.css'
 const Home = () => {
   // throw new Error('error in home page');
-  return <div>Hello World!</div>;
+  return <div className={styles.container}>
+    <div className={styles.textcontainer}>
+      <h1 className={styles.heading}>Create Thoughts Agency.</h1>
+      <p className={styles.desc}>These fruits will make you feel alive. This is an example page. It’s different from a blog post because it will stay in one place</p>
+      <div className={styles.buttons}>
+        <button className={styles.button}>Learn More</button>
+        <button className={styles.button}>Contact</button>
+      </div>
+      <div className={styles.brands}>
+        <Image className={styles.brandimage} src="/brands.png" alt='' fill />
+      </div>
+
+    </div>
+    <div className={styles.imagecontainer}>
+      <Image className={styles.heroimage} src="/hero.gif" alt='' fill />
+    </div>
+  </div>;
 };
 
 export default Home;
