d2e2448953ed5d153109e56cb2e89074d97a397b
AzureAD\FrankZhangMiningSkil <frank.zhang.sa.au@gmail.com>
2024-08-14 10:20:50 +1000

    1. add logo home link;

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Links from './links/Links'
 import styles from './navbar.module.css'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.logo}>
+      <Link href='/' className={styles.logo}>
         Logo
-      </div>
+      </Link>
       <div>
         <Links />
       </div>
