98757bf7293053a660380d0f151a8238c229e3c9
AzureAD\FrankZhangMiningSkil <frank.zhang.sa.au@gmail.com>
2024-08-14 12:57:20 +1000

    1. Finish about page layout;

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -4,11 +4,34 @@ import Image from 'next/image';
 
 const AboutPage = () => {
   return (
-    <div className={styles.imageContainer}>
-      {/* <Image src="/about.png"  alt='' width={500} height={500}/> */}
-      <Image src="https://staging.miningskills.com.au/wp-content/uploads/2024/05/site-icon.png"  alt='' fill />
+    <div className={styles.container}>
+      <div className={styles.textcontainer}>
+        <h2 className={styles.subheading}>About Agency</h2>
+        <h1 className={styles.heading}>We create digital ideas that are bigger, bolder, braver and better.</h1>
+        <p className={styles.desc}> We create digital ideas that are bigger, bolder, braver and better. We
+          believe in good ideas flexibility and precission We’re world’s Our
+          Special Team best consulting & finance solution provider. Wide range
+          of web and software development services.</p>
+        <div className={styles.boxes}>
+           <div className={styles.box}>
+              <h2 className={styles.boxNunber}>10+</h2>
+              <p className={styles.boxDesc}>Year of experience</p>
+          </div>
+          <div className={styles.box}>
+              <h2 className={styles.boxNunber}>10+</h2>
+              <p className={styles.boxDesc}>Year of experience</p>
+          </div>
+          <div className={styles.box}>
+              <h2 className={styles.boxNunber}>10+</h2>
+              <p className={styles.boxDesc}>Year of experience</p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.imagecontainer}>
+        <Image className={styles.aboutImage} src="/about.png" alt='' fill />
+      </div>
     </div>
-  )
+  );
 }
 
 export default AboutPage
