8d25e6e755078e66aa6e466653e58326dec03361
AzureAD\FrankZhangMiningSkil <frank.zhang.sa.au@gmail.com>
2024-08-14 10:02:31 +1000

    1. add images; 2. config next.config.js,so we can use external images;

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images:{
+        remotePatterns:[
+            {
+                protocol: 'https',
+                hostname: 'staging.miningskills.com.au'
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import styles from "./about.module.css";
+import Image from 'next/image';
 
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
+    <div className={styles.imageContainer}>
+      {/* <Image src="/about.png"  alt='' width={500} height={500}/> */}
+      <Image src="https://staging.miningskills.com.au/wp-content/uploads/2024/05/site-icon.png"  alt='' fill />
+    </div>
   )
 }
 
