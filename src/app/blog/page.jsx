6345263817c74a20b4238c5580990a5a2211129a
frankzhang <frank.zhang.sa.au@gmail.com>
2024-08-16 17:41:32 +1000

    try push

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,34 +1,32 @@
-import React from 'react'
+import React from "react";
 import styles from "./blog.module.css";
-import BlogCard from '@/components/blogCard/BlogCard';
+import BlogCard from "@/components/blogCard/BlogCard";
 
 const getData = async () => {
   // use cache:  {cache: "force-cache"} : DEFAULT
   // not use cache:  {cache: "no-store"}
   // {next: {revalidate: 3600}}:  every 1 hour fetch new data
-  const result = await fetch('https://jsonplaceholder.typicode.com/posts', { cache: "force-cache" });
+  const result = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    cache: "force-cache",
+  });
   if (!result.ok) {
-    throw new Error('error in blog page fetching data');
+    throw new Error("error in blog page fetching data");
   }
   return result.json();
-}
+};
 
 const BlogPage = async () => {
   const posts = await getData();
   // console.log(posts)
   return (
     <div className={styles.container}>
-      {
-        posts.map(
-          (post) => (
-            <div className={styles.post} key={post.id}>
-              <BlogCard post={post} />
-            </div>
-          )
-        )
-      }
+      {posts.map((post) => (
+        <div className={styles.post} key={post.id}>
+          <BlogCard post={post} />
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default BlogPage 
+export default BlogPage;
