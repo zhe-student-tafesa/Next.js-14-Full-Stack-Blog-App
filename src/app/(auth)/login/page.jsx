8ce5cae55a86320214b95ec54c9d9e42fa924ea6
AzureAD\FrankZhangMiningSkil <frank.zhang.sa.au@gmail.com>
2024-08-26 15:32:35 +1000

    1. manully edit SEO title and description

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import styles from "./login.module.css";
 
+export const metadata = {
+  title: 'Login Page | NextJS 14 Fullstack Blog',
+  description: 'Login Page Description',
+}
+
 const LoginPage = () => { 
   return (
     <div>LoginPage</div>
