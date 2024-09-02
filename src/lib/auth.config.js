ec158e2a149a9334bd3060d532faac58cd606d62
AzureAD\FrankZhangMiningSkil <frank.zhang.sa.au@gmail.com>
2024-09-02 16:11:50 +1000

    user000:  undefined  ??  WHY  ? ?

--- src/lib/auth.config.js
+++ src/lib/auth.config.js
@@ -2,12 +2,34 @@
 
 export const authConfig = {
     pages: {
-        signIn: "/login" 
+        signIn: "/login"
     },
     providers: [],
     callbacks: {
-        authorized({auth, request}){
-            return false
+        // Begin: add user info inside this session 
+        /// update token
+        async jwt({ token, user }) {
+            console.log("user000: ", user)
+            if(user){
+                console.log("user123: ", user)
+                token.id = user.id;
+                token.isAdmin= user.isAdmin;
+            }
+            return token;
+        },
+        /// use updated token: update session
+        async session({ session, token }) {
+            if(token){
+                console.log("token123: ", token)
+                session.user.id = token.id;
+                session.user.isAdmin= token.isAdmin;
+            }
+            return session;
+        },
+        // End: add user info inside this session 
+        authorized({ auth, request }) {
+            console.log("auth: ", auth)
+            return true
         }
     }
 }
