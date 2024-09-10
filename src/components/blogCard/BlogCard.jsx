f4103999cf3e3e585e1b3306e24af71188dab602
AzureAD\FrankZhangMiningSkil <frank.zhang.sa.au@gmail.com>
2024-09-10 14:56:30 +1000

    1. use post.createdAt to replace fake date;

--- src/components/blogCard/BlogCard.jsx
+++ src/components/blogCard/BlogCard.jsx
@@ -11,11 +11,11 @@ const BlogCard = ({ post }) => {
           <Image
             className={styles.img}
             alt=""
-            src= {post.img? post.img:"https://staging.miningskills.com.au/wp-content/uploads/2024/05/Post-6.jpg"}
+            src={post.img ? post.img : "https://staging.miningskills.com.au/wp-content/uploads/2024/05/Post-6.jpg"}
             fill
           />
         </div>
-        <span className={styles.date}>12.31.2024</span>
+        <span className={styles.date}>{post.createdAt.toString().slice(0, 10).replaceAll('-', '.')}</span>
       </div>
       <div className={styles.bottom}>
         <h3 className={styles.title}>{post.title}</h3>
