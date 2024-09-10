8ca165c9d5106733913fd278bd95ee95252054ec
AzureAD\FrankZhangMiningSkil <frank.zhang.sa.au@gmail.com>
2024-09-10 14:41:35 +1000

    1. Why no user id in Session? 2. Use DB to query user id;

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -4,13 +4,27 @@ import AdminUser from '@/components/adminUser/AdminUser';
 import AdminUserForm from '@/components/adminUserForm/AdminUserForm';
 import AdminPost from '@/components/adminPost/AdminPost';
 import AdminPostForm from '@/components/adminPostForm/AdminPostForm';
+import { auth } from '@/lib/auth';
+import { connectToDB } from '@/lib/utils';
+import { User } from '@/lib/models';
 
 export const metadata = {
   title: 'Admin Page',
   description: 'Admin Page Description',
 }
 
-const AdminPage = () => {
+const AdminPage = async () => {
+  const session = await auth();
+  let userId = '';
+  try {
+    connectToDB();
+    const user = await User.findOne({ email: session?.user?.email })
+    userId = user.id;
+
+  } catch (error) {
+    console.log(error, 'fail to query admin user')
+  }
+  // console.log("user.id??: ", userId)
   return (
     <div className={styles.container}>
       <div className={styles.containerPost}>
@@ -19,7 +33,7 @@ const AdminPage = () => {
             <AdminPost />
           </Suspense>
         </div>
-        <AdminPostForm className={styles.postForm} />
+        <AdminPostForm className={styles.postForm} userId={userId === '' ? "66d6a8cbd94cb3840db3af6d" : userId} />
       </div>
 
 
--- src/components/adminPostForm/AdminPostForm.jsx
+++ src/components/adminPostForm/AdminPostForm.jsx
@@ -4,15 +4,16 @@ import styles from "./adminPostForm.module.css"
 import React from 'react'
 import { useFormState } from "react-dom";
 
-const AdminPostForm = () => {
+const AdminPostForm = ({ userId }) => {
   const [state, formAction] = useFormState(createPost, undefined);
+  
   return (
     <form className={styles.container} action={formAction}>
       <h1>Add New Post</h1>
       <input type="text" name="title" placeholder="title"></input>
-      <textarea type="text" name="desc" placeholder="description" rows={10}/>
+      <textarea type="text" name="desc" placeholder="description" rows={10} />
       <input type="text" name="img" placeholder="image"></input>
-      <input type="text" name="userId" placeholder="user Id"></input>
+      <input type="hidden" name="userId" value={userId}></input>
       <input type="text" name="slug" placeholder="slug"></input>
       <button>Create Post</button>
       {state?.error && state.error}
